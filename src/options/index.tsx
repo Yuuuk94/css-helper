4dc7a3ecd92d690a7778690b7cdecce9a7751c72
May <may@vplate.io>
2024-10-25 19:24:10 +0900

    update setting chrome extendtion

---
+++ src/options/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Options from "./conponents";
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <React.StrictMode>
+    <Options />
+  </React.StrictMode>
+);
